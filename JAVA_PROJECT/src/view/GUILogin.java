d1ffd255571f374c66a68ccc3d57d0c3fc20eace
SM008 <SM008@DESKTOP-H83GDUP>
2020-01-13 14:48:18 +0900

    login

--- JAVA_PROJECT/src/view/GUILogin.java
+++ JAVA_PROJECT/src/view/GUILogin.java
@@ -1,10 +1,99 @@
 package view;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.SystemColor;
+
 public class GUILogin {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUILogin window = new GUILogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the application.
+	 */
+	public GUILogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setFont(new Font("∏º¿∫ ∞ÌµÒ", Font.PLAIN, 12));
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(175, 0, 434, 99);
+		frame.getContentPane().add(panel);
+		panel.setLayout(new CardLayout(0, 0));
+		
+		JLabel label = new JLabel("\uAC00\uBCF4\uC790");
+		label.setFont(new Font("πÆ√º∫Œ æ≤±‚ »Í∏≤√º", Font.BOLD, 40));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(label, "name_1448421216090600");
+		
+		textField = new JTextField();
+		textField.setBounds(297, 214, 312, 61);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(297, 339, 312, 61);
+		frame.getContentPane().add(textField_1);
+		
+		JButton login = new JButton("\uB85C\uADF8\uC778");
+		login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		login.setBounds(335, 457, 114, 37);
+		frame.getContentPane().add(login);
+		
+		JLabel lblNewLabel = new JLabel("\uC544\uC774\uB514");
+		lblNewLabel.setFont(new Font("∏º¿∫ ∞ÌµÒ", Font.PLAIN, 20));
+		lblNewLabel.setBounds(175, 217, 95, 46);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638");
+		lblNewLabel_1.setFont(new Font("∏º¿∫ ∞ÌµÒ", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(175, 352, 165, 27);
+		frame.getContentPane().add(lblNewLabel_1);
+	}
 }
