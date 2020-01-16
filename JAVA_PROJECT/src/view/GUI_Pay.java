39d5a9a8e5e896aca0ec51aca4e8506c13eed21b
SM008 <SM008@DESKTOP-H83GDUP>
2020-01-16 11:01:28 +0900

    GUIpay

--- JAVA_PROJECT/src/view/GUI_Pay.java
+++ JAVA_PROJECT/src/view/GUI_Pay.java
@@ -9,24 +9,28 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import controller.MemberManagementService;
 import model.Book;
 import model.Member;
 import model.MemberDAO;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GUI_Pay {
-
+//////////////////service 객체 생성 -> book객체를 service의 메소드에 넘겨줌
+	MemberManagementService service = new MemberManagementService();
 	private JFrame frame;
+	private JLabel dep;
+	private JLabel depT;
+	private JLabel arr;
+	private JLabel arrT;
+	private JLabel servnum;
+	private JLabel num;
+	private JLabel type;
+	private JLabel grade;
+	private JLabel person;
+	private JLabel fare;
 
-// 객체생성
-	MemberDAO dao = new MemberDAO();
-// 객체생성
-	Book bookUser;
-	
-	
-
-	
-	
-	
 	/**
 	 * Launch the application.
 	 */
@@ -35,7 +39,7 @@ public class GUI_Pay {
 	 * Create the application.
 	 */
 	public GUI_Pay(Member loginUser) {
-	
+
 		initialize(loginUser);
 		frame.setVisible(true);
 	}
@@ -48,86 +52,97 @@ public class GUI_Pay {
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, "name_1614660541439400");
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("출발");
 		lblNewLabel.setBounds(55, 49, 57, 15);
 		panel.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("도착");
 		lblNewLabel_1.setBounds(55, 93, 57, 15);
 		panel.add(lblNewLabel_1);
-		
+
 		JLabel serv_num = new JLabel("운행정보");
 		serv_num.setBounds(55, 205, 57, 15);
 		panel.add(serv_num);
-		
+
 		JLabel per = new JLabel("인원수");
 		per.setBounds(55, 510, 57, 15);
 		panel.add(per);
-		
+
 		JLabel lblNewLabel_4 = new JLabel("교통수단");
 		lblNewLabel_4.setBounds(55, 295, 57, 15);
 		panel.add(lblNewLabel_4);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("차량번호");
 		lblNewLabel_5.setBounds(363, 205, 57, 15);
 		panel.add(lblNewLabel_5);
-		
+
 		JButton btnNewButton = new JButton("결제하기");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+//////// 결제하기 버튼 클릭시 book객체를 생성하여 예매정보 생성
+///////////////////////////BOOk 객체생성
+				Book book = new Book(booknum, dep, arr, servnum, depT, arrT, person, fare);
+
+				int rows = service.bookingPayment(book);
+
+			}
+		});
 		btnNewButton.setBounds(631, 506, 97, 23);
 		panel.add(btnNewButton);
-		
+
 		JLabel lblNewLabel_6 = new JLabel("총 금액");
 		lblNewLabel_6.setBounds(326, 510, 57, 15);
 		panel.add(lblNewLabel_6);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("차량등급");
 		lblNewLabel_2.setBounds(363, 295, 57, 15);
 		panel.add(lblNewLabel_2);
-		
-		JLabel dep = new JLabel("New label");
+
+		dep = new JLabel("New label");
 		dep.setBounds(193, 49, 161, 15);
 		panel.add(dep);
-		
-		JLabel depT = new JLabel("New label");
+
+		depT = new JLabel("New label");
 		depT.setBounds(470, 49, 161, 15);
 		panel.add(depT);
-		
-		JLabel arr = new JLabel("New label");
+
+		arr = new JLabel("New label");
 		arr.setBounds(193, 93, 161, 15);
 		panel.add(arr);
-		
-		JLabel arrT = new JLabel("New label");
+
+		arrT = new JLabel("New label");
 		arrT.setBounds(470, 93, 161, 15);
 		panel.add(arrT);
-		
-		JLabel servnum = new JLabel("New label");
+
+		servnum = new JLabel("New label");
 		servnum.setBounds(156, 205, 161, 15);
 		panel.add(servnum);
-		
-		JLabel num = new JLabel("New label");
+
+		num = new JLabel("New label");
 		num.setBounds(489, 205, 161, 15);
 		panel.add(num);
-		
-		JLabel type = new JLabel("New label");
+
+		type = new JLabel("New label");
 		type.setBounds(156, 295, 161, 15);
 		panel.add(type);
-		
-		JLabel grade = new JLabel("New label");
+
+		grade = new JLabel("New label");
 		grade.setBounds(489, 295, 161, 15);
 		panel.add(grade);
-		
-		JLabel person = new JLabel("New label");
+
+		person = new JLabel("New label");
 		person.setBounds(111, 510, 161, 15);
 		panel.add(person);
-		
-		JLabel fare = new JLabel("New label");
+
+		fare = new JLabel("New label");
 		fare.setBounds(406, 510, 161, 15);
 		panel.add(fare);
+
 	}
 }
-
